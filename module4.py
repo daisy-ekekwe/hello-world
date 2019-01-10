48b0f1ea805eeb58e5cacb9da226329cc8366615
Daisy Ekekwe <daisy.kester@gmail.com>
2019-01-10 22:44:12 +0100

    module4assignment

---
+++ module4.py
@@ -0,0 +1,5 @@
+name = input('please enter your name: ')
+age = input('please enter your age: ')
+default = int(100)
+
+print('Hello', name, 'you will be a 100 years in', default - int(age), 'years');
