a820bd8f5fa485644bb386912400c4e3a8637aa4
Daisy Ekekwe <daisy.kester@gmail.com>
2019-01-23 22:25:04 +0100

    Assignment

---
+++ Panda_series_assignment.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from collections import OrderedDict
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+revenue = [145, 760, 861, 917, 805, 810, 114, 976, 103, 143, 107, 154]
+expenses = [120, 569, 123, 120, 865, 840, 328, 582, 697, 166, 100, 380]
+profit = np.array(revenue) - np.array(expenses)
+Fin_status = OrderedDict(zip(months, profit))
+pd_Fin_status = pd.Series(Fin_status)
+print(pd_Fin_status);
---
+++ line_graph.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from collections import OrderedDict
+months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV']
+revenue = [145, 760, 861, 917, 805, 810, 114, 976, 103, 143, 107, 154]
+expenses = [120, 569, 123, 120, 865, 840, 328, 582, 697, 166, 100, 380]
+profit = np.array(revenue) - np.array(expenses)
+Fin_status = OrderedDict(zip(months, profit))
+pd_Fin_status = pd.Series(Fin_status)
+pd_Fin_status.plot()
+plt.title('Profit Per Month')
+plt.xlabel('Months')
+plt.ylabel('Profit')
+plt.show()
