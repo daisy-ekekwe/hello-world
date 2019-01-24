00397477be4297d1da75807240185138c6a4412e
daisy-ekekwe <46576450+daisy-ekekwe@users.noreply.github.com>
2019-01-24 09:51:34 +0100

    Update line_graph.py

--- line_graph.py
+++ line_graph.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from collections import OrderedDict
-months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV']
+months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV' 'DEC']
 revenue = [145, 760, 861, 917, 805, 810, 114, 976, 103, 143, 107, 154]
 expenses = [120, 569, 123, 120, 865, 840, 328, 582, 697, 166, 100, 380]
 profit = np.array(revenue) - np.array(expenses)
